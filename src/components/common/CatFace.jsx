e75569d53a1f6516ab0bf9bd7c6ee341c8479c26
Josh Mini <mini@JoshdeMac-mini.local>
2026-08-14 17:58:06 +0800

    chore(profile): remove retired template components

--- src/components/common/CatFace.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CatFace = ({ className = "", size = "text-2xl" }) => (
-  <span className={`${size} ${className}`} style={{ display: 'inline-block' }} aria-hidden="true">🐱</span>
-);
-
-export default CatFace;
